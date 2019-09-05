3c172fe636a4667bf10c36fafeffff2c8b24d72b
javidiazdom <javidiazdom@gmail.com>
2019-09-05 19:35:59 +0100

    Portfolio upload
    Hi, this is my first upload. Im working on developing a home page to showcase my developer portfolio and skills.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ReactComponent as Semicircle} from './svg/semicircle.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className = "home">
+      <div className = "line">
+        <div className = "bg"/>
+      </div>
+      {/*Here goes the js animation*/}
     </div>
+    <div className = "transition-circles-container">
+      <Semicircles/>
+    </div>
+    <div className = "info">
+
+    </div>
+    </>
+  );
+}
+
+function Semicircles() {
+  return (
+    <>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+      <div className="circle">
+        <Semicircle></Semicircle>
+      </div>
+    </>
   );
 }
 
