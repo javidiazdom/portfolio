0cf400f72d11a2ead73748d677995c6435f93f9d
javidiazdom <javidiazdom@gmail.com>
2022-02-18 01:17:00 +0000

    :sparkles: Initial configs and sketching

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,12 @@ module.exports = {
       title: `my-portfolio`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass", {
+    resolve: "gatsby-plugin-react-svg",
+    options: {
+      rule: {
+        include: /assets/ // See below to configure properly
+      }
+    }
+  }]
 };
---
+++ src/components/body.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const body = () => {
+    return (
+        <div class="body">
+        </div>
+    )
+}
+
+export default body;
